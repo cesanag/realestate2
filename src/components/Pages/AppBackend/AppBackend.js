4e3da13b20729fec53d6334e985f7e2570095197
AleC <cesaless@gmail.com>
2020-10-14 21:48:49 +1100

    some instructions added

--- src/components/Pages/AppBackend/AppBackend.js
+++ src/components/Pages/AppBackend/AppBackend.js
@@ -4,6 +4,8 @@ import React, { useState } from 'react';
 //Import Style
 import './AppBackend.scss';
 
+//Import data
+import propertiesList from '../../../backend/listaImmobili.json'
 //Import ReusableComponents
 import Header from 'components/ReusableComponents/Header/Header';
 import Navigator from 'components/ReusableComponents/Navigator/Navigator';
@@ -13,11 +15,28 @@ import Posting from 'components/ReusableComponents/Posting/Posting'
 
 function AppBackend() {
 
+  
+  console.log(propertiesList)
   //Set Input Value State
   const [inputAddress, setInputAddress] = useState()
   function handleInputs(newInputAddress) {
+    //console.log(newInputAddress);
     setInputAddress(newInputAddress)
   }
+
+  console.log(inputAddress);
+
+  //IDEAL: add property to propertiesList State
+    //user fills up form to add property
+    //user clicks 'submit property'
+    //api call is made with property data as parameters
+    //backend receives data from api call
+    //backend validates data against requirements
+    //backend adds property to properties list
+
+  //DEMO PATH
+  //modify inputs so that street number and street name are separate
+  //on click, array.push(data) on propertiesList.listaImmobili
 
   return (
     <div className="App">
