921008caa733759885c4cac939df3dd154519f7d
cesanag <gcesa00@gmail.com>
2020-11-13 16:13:09 +0100

    update

--- src/pages/appFrontend/appFrontend.js
+++ src/pages/appFrontend/appFrontend.js
@@ -1,5 +1,5 @@
 //Import React
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
 //Import Style
 import './appFrontend.scss';
@@ -17,8 +17,6 @@ import FilterButton from 'components/filterButton/filterButton'
 import FilterButtonModal from 'components/filterButtonModal/filterButtonModal'
 import FilterButtonSpacer from 'components/filterButtonSpacer/filterButtonSpacer'
 
-//import Slider from 'components/slider/slider'
-
 //Import Icons
 import { ReactComponent as IconFilters } from 'assets/icon/icon-filters.svg'
 import { ReactComponent as IconMap } from 'assets/icon/icon-map.svg'
@@ -29,187 +27,146 @@ import { Copy } from 'utilities/copy'
 
 export default function AppFrontend() {
 
-  // State
-  const [isFilterHidden, setIsFilterHidden] = useState(true);
-  const [isMapHidden, setIsMapHidden] = useState(false);
-  const [isListHidden, setIsListHidden] = useState(true);
+// State
+const [isFilterHidden, setIsFilterHidden] = useState(true);
+const [isMapHidden, setIsMapHidden] = useState(false);
+const [isListHidden, setIsListHidden] = useState(true);
 
-  const [isType1ButtonClicked, setIsType1ButtonClicked] = useState(false)
-  const [isType2ButtonClicked, setIsType2ButtonClicked] = useState(false)
-  const [isType3ButtonClicked, setIsType3ButtonClicked] = useState(false)
-  const [isType4ButtonClicked, setIsType4ButtonClicked] = useState(false)
-
-
-  // State Functions
-
-  function handleFilterClick() {
-    setIsFilterHidden(!isFilterHidden)
-  }
-
-  function handleMapClick() {
-    setIsMapHidden(!isMapHidden)
-  }
-
-  function handleIsType1ButtonClicked() {
-    setIsType1ButtonClicked(!isType1ButtonClicked)
-  }
-  
-  function handleIsType2ButtonClicked() {
-    setIsType2ButtonClicked(!isType2ButtonClicked)
-  }
-  function handleIsType3ButtonClicked() {
-    setIsType3ButtonClicked(!isType3ButtonClicked)
-  }
-  function handleIsType4ButtonClicked() {
-    setIsType4ButtonClicked(!isType4ButtonClicked)
-  }
-
-    //chiamare le cose ESPLICITAMENTE
-  //function: handleHomeFilterClick
-  //Copy: homeFilterButton, officeFilterButton
-  //state: isHomeFilterEnabled setIsHomeFilterEnabled
-  //se è boolean: is...
-  //se è array/lista: propertyList
-
-  // [{text: 'casa', onClickHandler: () => {}, isToggled: false},{…},{…}]
-  // <typeFilters filtersState= [oggetto Sopra]>
-  // array.map(filterState => <filterButton>)}
-
-  //sistemare colors con sistema ale => usare background url() e content (placeholder)
-  //backgroundImage: 'svgPath';content: '[]',::before , ::after
+const [isType1ButtonClicked, setIsType1ButtonClicked] = useState(false)
+const [isType2ButtonClicked, setIsType2ButtonClicked] = useState(false)
+const [isType3ButtonClicked, setIsType3ButtonClicked] = useState(false)
+const [isType4ButtonClicked, setIsType4ButtonClicked] = useState(false)
 
 
-  
+// State Functions
+
+function handleFilterClick() {
+  setIsFilterHidden(!isFilterHidden)
+}
+function handleMapClick() {
+  setIsMapHidden(!isMapHidden)
+}
+function handleIsType1ButtonClicked() {
+  setIsType1ButtonClicked(!isType1ButtonClicked)
+}
+function handleIsType2ButtonClicked() {
+  setIsType2ButtonClicked(!isType2ButtonClicked)
+}
+function handleIsType3ButtonClicked() {
+  setIsType3ButtonClicked(!isType3ButtonClicked)
+}
+function handleIsType4ButtonClicked() {
+  setIsType4ButtonClicked(!isType4ButtonClicked)
+}
+
   return (
     <div>
       <header className="section-header">
         <Header />
         <FilterButtonMain >
           <div className="buttonfilters-content">
-            
             <div className='filterbuttonmain-buttoncontainer'>
-              <SearchBar 
+              <SearchBar
                 text={Copy.search + " " + Copy.company + "..."}
-                />
+              />
               <LeftIconButton
-                icon= {<IconFilters />}
+                icon={<IconFilters />}
                 buttonText={Copy.filter}
                 onButtonClick={handleFilterClick}
-                isButtonClicked = {!isFilterHidden}
+                isButtonClicked={!isFilterHidden}
               />
               <LeftIconButton
                 icon={<IconMap />}
                 buttonText={Copy.map}
                 onButtonClick={handleMapClick}
-                isButtonClicked = {!isMapHidden}
+                isButtonClicked={!isMapHidden}
               />
               <div className="filterbuttonmain-list-container"
                 // onClick={() => setIsListHidden(false)}
                 onMouseEnter={() => setIsListHidden(false)}
-                onMouseLeave={() => setIsListHidden(true)}> 
-                <LeftIconButton 
+                onMouseLeave={() => setIsListHidden(true)}>
+                <LeftIconButton
                   icon={<IconList />}
                   buttonText={Copy.order}
-                  // isButtonClicked = {!isListHidden}
+                // isButtonClicked = {!isListHidden}
                 />
-                
-                <div className= {`filterbuttonmain-list-modal ${isListHidden && "is-hidden"}`}>
-                  <div className="filterbuttonmain-list-text">
-                  {Copy.orderByRelevance}
-                  </div>
-                  <div className="filterbuttonmain-list-text">
-                    {Copy.orderByName}
-                  </div>
-                  <div className="filterbuttonmain-list-text">
-                    {Copy.orderByPrice}
-                  </div>
+                <div className={`filterbuttonmain-list-modal ${isListHidden && "is-hidden"}`}>
+                  <LeftIconButton 
+                    buttonText={Copy.orderByRelevance}
+                  />
+                  <LeftIconButton 
+                    buttonText={Copy.orderByName}
+                  />
+                  <LeftIconButton 
+                    buttonText={Copy.orderByPrice}
+                  />
                 </div>
               </div>
-              
             </div>
           </div>
         </FilterButtonMain>
         <Filters isFilterHidden={isFilterHidden}>
           <div className="filters-type">
-              <div className="filterbutton-container">
-                {/*}
-                <div className="filters-name">
-                {Copy.type}
-                </div>
-                */}
-                <FilterButton
+            <div className="filterbutton-container">
+              <FilterButton
                 buttonText={Copy.type1}
                 onButtonClick={handleIsType1ButtonClicked}
-                isButtonClicked = {!isType1ButtonClicked}
-                // name: "isHomeFilterOn, isOfficeFilterOn
-                />
-                <FilterButton 
+                isButtonClicked={!isType1ButtonClicked}
+              />
+              <FilterButton
                 buttonText={Copy.type2}
                 onButtonClick={handleIsType2ButtonClicked}
-                isButtonClicked = {!isType2ButtonClicked}
-                />
-                <FilterButton 
+                isButtonClicked={!isType2ButtonClicked}
+              />
+              <FilterButton
                 buttonText={Copy.type3}
                 onButtonClick={handleIsType3ButtonClicked}
-                isButtonClicked = {!isType3ButtonClicked}
-                />
-                <FilterButton
+                isButtonClicked={!isType3ButtonClicked}
+              />
+              <FilterButton
                 buttonText={Copy.type4}
                 onButtonClick={handleIsType4ButtonClicked}
-                isButtonClicked = {!isType4ButtonClicked}
-                />
-                <FilterButtonSpacer 
-                />
-                <FilterButtonModal
+                isButtonClicked={!isType4ButtonClicked}
+              />
+              <FilterButtonSpacer
+              />
+              <FilterButtonModal
                 buttonText={Copy.price}
-                />
-                <FilterButtonModal
+              />
+              <FilterButtonModal
                 buttonText={Copy.dimension}
-                />
-                <FilterButtonModal
+              />
+              <FilterButtonModal
                 buttonText={Copy.rooms}
-                />
-                <FilterButtonModal
+              />
+              <FilterButtonModal
                 buttonText={Copy.bathrooms}
-                />
-                <NavigatorButton
-                    class="navigator-button button-green"
-                    buttonText= "Applica">
-                </NavigatorButton>
-                <NavigatorButton
-                    class="navigator-button button-red"
-                    buttonText= "Chiudi">
-                </NavigatorButton>
-              </div>
+              />
+              <NavigatorButton
+                class="navigator-button button-green"
+                buttonText="Applica">
+              </NavigatorButton>
+              <NavigatorButton
+                class={`navigator-button button-red ${!isFilterHidden}`}
+                buttonText="X"
+                onButtonClick={handleFilterClick}>
+              </NavigatorButton>
+            </div>
           </div>
-
         </Filters>
       </header>
-
-        <div className="section-main">
-          <div className="panel-container">
-            <PanelClose />
-            <PanelClose />
-            <PanelClose />
-            <PanelClose />
-            <PanelClose />
-          </div>
-          <div className="map-container">
-            <Map isMapHidden={isMapHidden}/>
-            {/* <MapContainer center={[45.5845, 9.2744]} zoom={17} scrollWheelZoom={false}>
-              <TileLayer
-                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-              />
-              <Marker position={[45.5845, 9.2744]}>
-              </Marker>
-            </MapContainer> */}
-          </div>
-
-          
+      <div className="section-main">
+        <div className="panel-container">
+          <PanelClose />
+          <PanelClose />
+          <PanelClose />
+          <PanelClose />
+          <PanelClose />
         </div>
+        <div className="map-container">
+          <Map isMapHidden={isMapHidden} />
+        </div>
+      </div>
     </div>
-    
   );
-  
 }
--- src/pages/backendHome/backendHome.js
+++ src/pages/backendHome/backendHome.js
@@ -1,5 +1,5 @@
 //Import React
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
 //Import Style
 import './backendHome.scss';
@@ -24,169 +24,152 @@ import { ReactComponent as IconList } from 'assets/icon/icon-list.svg'
 //Import Copy
 import { Copy } from 'utilities/copy'
 
-
 export default function BackendHome() {
 
-  // State
-  const [isFilterHidden, setIsFilterHidden] = useState(true);
-  const [isListHidden, setIsListHidden] = useState(true);
-  
-  const [isType1ButtonClicked, setIsType1ButtonClicked] = useState(false)
-  const [isType2ButtonClicked, setIsType2ButtonClicked] = useState(false)
-  const [isType3ButtonClicked, setIsType3ButtonClicked] = useState(false)
-  const [isType4ButtonClicked, setIsType4ButtonClicked] = useState(false)
-  
-   // State Functions
+// State
+const [isFilterHidden, setIsFilterHidden] = useState(true);
+const [isListHidden, setIsListHidden] = useState(true);
+const [isType1ButtonClicked, setIsType1ButtonClicked] = useState(false)
+const [isType2ButtonClicked, setIsType2ButtonClicked] = useState(false)
+const [isType3ButtonClicked, setIsType3ButtonClicked] = useState(false)
+const [isType4ButtonClicked, setIsType4ButtonClicked] = useState(false)
 
-   function handleFilterClick() {
+// State Functions
+
+function handleFilterClick() {
     setIsFilterHidden(!isFilterHidden)
-  }
-
-  function handleIsType1ButtonClicked() {
+}
+function handleIsType1ButtonClicked() {
     setIsType1ButtonClicked(!isType1ButtonClicked)
-  }
-  
-  function handleIsType2ButtonClicked() {
+}
+function handleIsType2ButtonClicked() {
     setIsType2ButtonClicked(!isType2ButtonClicked)
-  }
-  function handleIsType3ButtonClicked() {
+}
+function handleIsType3ButtonClicked() {
     setIsType3ButtonClicked(!isType3ButtonClicked)
-  }
-  function handleIsType4ButtonClicked() {
+}
+function handleIsType4ButtonClicked() {
     setIsType4ButtonClicked(!isType4ButtonClicked)
-  }
+}
 
-  return (
+return (
     <div>
         <header className="section-header">
             <Header />
             <Navigator>
                 <NavigatorButton
                     class="navigator-button button-green"
-                    buttonText= "Crea nuovo annuncio"
+                    buttonText="Crea nuovo annuncio"
                 />
                 <NavigatorButton
                     class="navigator-button button-green"
-                    buttonText= "Elimina annunci"
+                    buttonText="Elimina annunci"
                 />
-
                 <NavigatorButton
                     class="navigator-button button-red"
-                    buttonText= "Salva"
+                    buttonText="Salva"
                 />
             </Navigator>
             <FilterButtonMain >
                 <div className="buttonfilters-content">
-                    
                     <div className='filterbuttonmain-buttoncontainer'>
-                    <SearchBar 
-                        text="Cerca tra gli annunci"/>
-                    <LeftIconButton
-                        icon= {<IconFilters />}
-                        buttonText={Copy.filter}
-                        onButtonClick={handleFilterClick}
-                        isButtonClicked = {!isFilterHidden}
-                    />
-                    <div className="filterbuttonmain-list-container"
-                        // onClick={() => setIsListHidden(false)}
-                        onMouseEnter={() => setIsListHidden(false)}
-                        onMouseLeave={() => setIsListHidden(true)}> 
-                        <LeftIconButton 
-                        icon={<IconList />}
-                        buttonText={Copy.order}
-                        // isButtonClicked = {!isListHidden}
+                        <SearchBar
+                            text="Cerca tra gli annunci" />
+                        <LeftIconButton
+                            icon={<IconFilters />}
+                            buttonText={Copy.filter}
+                            onButtonClick={handleFilterClick}
+                            isButtonClicked={!isFilterHidden}
                         />
-                        
-                        <div className= {`filterbuttonmain-list-modal ${isListHidden && "is-hidden"}`}>
-                        <div className="filterbuttonmain-list-text">
-                        {Copy.orderByRelevance}
+                        <div className="filterbuttonmain-list-container"
+                            onMouseEnter={() => setIsListHidden(false)}
+                            onMouseLeave={() => setIsListHidden(true)}>
+                            <LeftIconButton
+                                icon={<IconList />}
+                                buttonText={Copy.order}
+                            // isButtonClicked = {!isListHidden}
+                            />
+
+                            <div className={`filterbuttonmain-list-modal ${isListHidden && "is-hidden"}`}>
+                                <div className="filterbuttonmain-list-text">
+                                    {Copy.orderByRelevance}
+                                </div>
+                                <div className="filterbuttonmain-list-text">
+                                    {Copy.orderByName}
+                                </div>
+                                <div className="filterbuttonmain-list-text">
+                                    {Copy.orderByPrice}
+                                </div>
+                            </div>
                         </div>
-                        <div className="filterbuttonmain-list-text">
-                            {Copy.orderByName}
-                        </div>
-                        <div className="filterbuttonmain-list-text">
-                            {Copy.orderByPrice}
-                        </div>
-                        </div>
-                    </div>
-                    
                     </div>
                 </div>
-                </FilterButtonMain>
-                <Filters isFilterHidden={isFilterHidden}>
+            </FilterButtonMain>
+            <Filters isFilterHidden={isFilterHidden}>
                 <div className="filters-type">
                     <div className="filterbutton-container">
-                        {/*}
-                        <div className="filters-name">
-                        {Copy.type}
-                        </div>
-                        */}
                         <FilterButton
-                        buttonText={Copy.type1}
-                        onButtonClick={handleIsType1ButtonClicked}
-                        isButtonClicked = {!isType1ButtonClicked}
+                            buttonText={Copy.type1}
+                            onButtonClick={handleIsType1ButtonClicked}
+                            isButtonClicked={!isType1ButtonClicked}
                         // name: "isHomeFilterOn, isOfficeFilterOn
                         />
-                        <FilterButton 
-                        buttonText={Copy.type2}
-                        onButtonClick={handleIsType2ButtonClicked}
-                        isButtonClicked = {!isType2ButtonClicked}
-                        />
-                        <FilterButton 
-                        buttonText={Copy.type3}
-                        onButtonClick={handleIsType3ButtonClicked}
-                        isButtonClicked = {!isType3ButtonClicked}
+                        <FilterButton
+                            buttonText={Copy.type2}
+                            onButtonClick={handleIsType2ButtonClicked}
+                            isButtonClicked={!isType2ButtonClicked}
                         />
                         <FilterButton
-                        buttonText={Copy.type4}
-                        onButtonClick={handleIsType4ButtonClicked}
-                        isButtonClicked = {!isType4ButtonClicked}
+                            buttonText={Copy.type3}
+                            onButtonClick={handleIsType3ButtonClicked}
+                            isButtonClicked={!isType3ButtonClicked}
                         />
-                        <FilterButtonSpacer 
+                        <FilterButton
+                            buttonText={Copy.type4}
+                            onButtonClick={handleIsType4ButtonClicked}
+                            isButtonClicked={!isType4ButtonClicked}
                         />
-                        <FilterButtonModal
-                        buttonText={Copy.price}
-                        />
-                        <FilterButtonModal
-                        buttonText={Copy.dimension}
-                        />
-                        <FilterButtonModal
-                        buttonText={Copy.rooms}
+                        <FilterButtonSpacer
                         />
                         <FilterButtonModal
-                        buttonText={Copy.bathrooms}
+                            buttonText={Copy.price}
+                        />
+                        <FilterButtonModal
+                            buttonText={Copy.dimension}
+                        />
+                        <FilterButtonModal
+                            buttonText={Copy.rooms}
+                        />
+                        <FilterButtonModal
+                            buttonText={Copy.bathrooms}
                         />
                         <NavigatorButton
                             class="navigator-button button-green"
-                            buttonText= "Applica">
+                            buttonText="Applica">
                         </NavigatorButton>
                         <NavigatorButton
-                            class= {`navigator-button button-red ${!isFilterHidden}`}
-                            buttonText= "X"
-                            onButtonClick={handleFilterClick}
-                            >
-                                
+                            class={`navigator-button button-red ${!isFilterHidden}`}
+                            buttonText="X"
+                            onButtonClick={handleFilterClick}>
                         </NavigatorButton>
                     </div>
                 </div>
-
-                </Filters>
-            
+            </Filters>
         </header>
         <div className="section-main">
             <div className="panel-container">
-            <PanelClose 
+                <PanelClose
                 />
-            <PanelClose 
-            />
-            <PanelClose 
+                <PanelClose
                 />
-            <PanelClose 
-            />
-            <PanelClose 
-            />
+                <PanelClose
+                />
+                <PanelClose
+                />
+                <PanelClose
+                />
             </div>
         </div>
     </div>
-  );
+);
 }
